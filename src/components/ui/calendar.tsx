f61e4f8a2360fb067ad645d1ee126a9835f11314
Vidhwan <vidhwan06@gmail.com>
2025-12-11 13:34:04 +0530

    fix: update calendar component for react-day-picker v9

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,13 +54,16 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
+        Chevron: ({ className, ...props }) => {
+          const isNext = props.dir === "next";
+          return isNext ? (
+            <ChevronRight className={cn("h-4 w-4", className)} {...props} />
+          ) : (
+            <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
+          );
+        },
       }}
+
       {...props}
     />
   )
